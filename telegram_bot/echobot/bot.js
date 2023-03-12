59d0dc5e22a8ae3b03cd4a8e1f332ffe8331d725
cryart <68236130+Gaming552@users.noreply.github.com>
2023-03-13 01:56:32 +0200

    Create bot.js

---
+++ telegram_bot/echobot/bot.js
@@ -0,0 +1,13 @@
+//1. we have to get our telepragoh library (class)
+const Telegraf = require('telegraf');
+
+
+//2. Creating a Constocter then tokin ID
+const bot = new Telegraf('1878559274:AAE0cQnERkIftx48occlHU54_jyrE9YQ8KA');
+
+
+
+
+
+//3. launch out bot 
+bot.launch(); 
