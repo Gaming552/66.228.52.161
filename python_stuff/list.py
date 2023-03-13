c038ecab64f31090cf7cb96ae3cb6b5b2f603610
cryart <68236130+Gaming552@users.noreply.github.com>
2023-03-13 02:03:17 +0200

    Create list.py

---
+++ python_stuff/list.py
@@ -0,0 +1,10 @@
+
+#Creaing a list
+#numbers = [1,2,3,4,5]
+
+#Using a contructor 
+#numbers = list((1,2,3,4,5))
+numbers = list((1,2,3,4,5))
+fruits = ['Apples','Oranges','Grapes','Pears', 12]
+
+print(fruits)
