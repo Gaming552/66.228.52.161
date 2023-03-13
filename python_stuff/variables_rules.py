70c80b2c279a82345e238b06d5a084f4e4ba31be
cryart <68236130+Gaming552@users.noreply.github.com>
2023-03-13 02:05:21 +0200

    Create variables_rules.py

---
+++ python_stuff/variables_rules.py
@@ -0,0 +1,26 @@
+
+
+x = 1               #intiger (int) 
+y = 2.5             #float  (decemal)
+name = 'Jesse'      #string (anythiny)
+is_cool = True      #bolean (True or False) Note have to be capital
+
+
+# Multiple assigment  
+x, y, name ,is_cool = (1, 25, 'Jesse', True)
+
+#print (x, y ,name, is_cool )
+
+#basic math 
+a = x + y
+
+#print(a)
+
+
+#check type
+#print(type(x))
+
+#casting
+x = str(x)
+print(type(x))
+
