9e597bcf74e2d29f721a9b7156f0181769953d12
cryart <68236130+Gaming552@users.noreply.github.com>
2023-03-13 02:04:57 +0200

    Create tuples_sets.py

---
+++ python_stuff/tuples_sets.py
@@ -0,0 +1,7 @@
+#A tuple is a collection which is ordered and unchangeable . Allows duplicate members 
+
+#fruit_tuple = ('Apples','Orange','Mango')
+
+fruit_tuple = tuple(('Apples','Orange','Mango'))
+
+print(fruit_tuple[1])
