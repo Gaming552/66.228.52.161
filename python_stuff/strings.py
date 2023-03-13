77926e68e7419843c7a7791049b5aa8794f573dc
cryart <68236130+Gaming552@users.noreply.github.com>
2023-03-13 02:03:43 +0200

    Create strings.py

---
+++ python_stuff/strings.py
@@ -0,0 +1,24 @@
+name = 'Jesse'
+age = 37
+
+#concatinate 
+#print('hello I am ' + name + ' and I am ' + str(age))
+
+
+# Arguments by position
+#print('{}, {}, {}'.format('a', 'b', 'c'))
+
+#print('My name is {name} and I am {age}'.format(name = 'jesse', age = 37) )
+
+#F-Strings (only in 3.6+)
+#print(f'My name is {name} and I am {age}'
+
+
+s = 'hello there world'
+
+#capitolize first letter 
+print(len(s))
+
+
+#replace 
+print(s.replace('world', 'everyone'))
